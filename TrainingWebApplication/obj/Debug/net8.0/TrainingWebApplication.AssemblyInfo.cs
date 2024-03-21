6bf4ed09f9ca24aa802da6d0dfd8c141225e5883
Deepthi.Shetty <Deepthi.Shetty@TL444>
2024-03-21 10:26:14 +0530

    updated visual studio

--- TrainingWebApplication/obj/Debug/net8.0/TrainingWebApplication.AssemblyInfo.cs
+++ TrainingWebApplication/obj/Debug/net8.0/TrainingWebApplication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TrainingWebApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9217c067702618002901434bd744db09bd29ffd8")]
 [assembly: System.Reflection.AssemblyProductAttribute("TrainingWebApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TrainingWebApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
